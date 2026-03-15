bb5bc7fa7a8a8e00418a65730dcb3921f846ed4d
Nex ZHu <hitnexup@gmail.com>
2026-03-15 21:28:20 +0800

    customize: docker-compooe.yaml: use Qdrant

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -a
 source ./.env
 set +a
 
 mkdir -p ${VOLUMES_ROOT}/data
 mkdir -p ${VOLUMES_ROOT}/postgres
+mkdir -p ${VOLUMES_ROOT}/qdrant/data
 mkdir -p ${VOLUMES_ROOT}/ollama
